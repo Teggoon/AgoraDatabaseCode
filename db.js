b78685274e1d6d5348e372de0b9b8bf8beb7fa4e
Haocheng Li <49082800+Teggoon@users.noreply.github.com>
2020-12-01 19:51:55 -0700

    updated db.js with nick's auth info

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase");
 
-const db = firebase.initializeApp({
+/*const db = firebase.initializeApp({
     apiKey: "AIzaSyAX14vtpXXgs3Nl3XR3o_ANAoN_r0GW8c0",
     authDomain: "agoradbtest.firebaseapp.com",
     databaseURL: "https://agoradbtest.firebaseio.com",
@@ -9,7 +9,13 @@ const db = firebase.initializeApp({
     messagingSenderId: "632535587209",
     appId: "1:632535587209:web:20c0b383299022d41f6958",
     measurementId: "G-SS09EBWP13"
+});*/
+const db = firebase.initializeApp({
+apikey: "AIzaSyB-4QRn4XGp2EVTcRxrDX8hf0VQ9jiu-GQ",
+  authDomain: "agora.bid",
+  databaseURL: "https://agora.bid/",
+  projectId: "agora-36616",
+  storageBucket: ""
 });
-
 
 module.exports = db;

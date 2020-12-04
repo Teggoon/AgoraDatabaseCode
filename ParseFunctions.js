8acac6dd1ee778829afc55e85cc6bf1079b88f89
Juan Ramirez <juanframirez64@gmail.com>
2020-12-04 11:55:38 -0800

    Fixed parsing to float/int issue

--- ParseFunctions.js
+++ ParseFunctions.js
@@ -23,8 +23,8 @@ class ParseFunctions {
 
             const newTradeObject =
                 {
-                    price: parseInt(price),
-                    size: parseInt(size)
+                    price: parseFloat(price),
+                    size: parseFloat(size)
                 };
 
             arrayOfParsedBids.push(newTradeObject);
@@ -38,8 +38,8 @@ class ParseFunctions {
 
             const newTradeObject =
                 {
-                    price: parseInt(price),
-                    size: parseInt(size)
+                    price: parseFloat(price),
+                    size: parseFloat(size)
                 };
 
             arrayOfParsedAsks.push(newTradeObject);
